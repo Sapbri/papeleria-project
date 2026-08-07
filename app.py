c9e4a10dc47ce44231aea1aea4704a7e778982d6
sapbri <92655888+Sapbri@users.noreply.github.com>
2026-08-06 19:55:52 -0500

    feat: cambio de clave completado, actualizacion de index y requirements.txt

--- app.py
+++ app.py
@@ -20,20 +20,82 @@ Session(app)
 # Le decimos a Flask que use las reglas de cierre de db.py:
 db.init_app(app)
 
+@app.after_request
+def after_request(response):
+    """Asegura que las respuestas no son guardadas en el navegador"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/", methods=["GET"])
 @login_required
 def index():
-    # Probamos la conexión haciendo un conteo de productos
-    conexion = db.get_db()
-    resultado = conexion.execute('SELECT COUNT(*) AS total FROM products').fetchone()
-    total = resultado['total'] 
+    return render_template("index.html")
 
-    return f"<h1>¡Conexión Exitosa!</h1><p>Productos en el sistema: {total}</p>"
+
+@app.route("/cambiar_clave", methods=["GET", "POST"])
+@login_required
+def change_password():
+    '''Cambiar la contraseña del usuario'''
+
+    # Revisar si la peticion es POST o GET
+    if request.method == "POST":
+
+        # Extraer la informacion del form
+        current_password = request.form.get("current_password").strip()
+        new_password = request.form.get("new_password").strip()
+        confirmation_password = request.form.get("confirmation_password").strip()
+
+        # Comprobar que no hay espacios adicionales o que los campos estan en blanco
+        if not current_password:
+            return apology("Debe digitar su contraseña actual", 400)
+
+        if not new_password:
+            return apology("Debe digitar su nueva contraseña", 400)
+
+        if not confirmation_password:
+            return apology("Debe digitar su nueva contraseña para confirmarla", 400)
+
+        # Comprobar que la contraseña escrita es la misma que la contraseña confirmada.
+        if new_password != confirmation_password:
+            return apology("Su nueva contraseña NO es igual. Comprueba que la contraseña digitada en el campo de confimracion sea la misma que la del campo de nueva contraseña.", 403)
+
+        # Comprobar que la contraseña escrita es diferente a la contraseña anterior.
+        if current_password == new_password:
+            return apology("Su nueva contraseña no puede ser la misma que su contraseña anterior. Digite una contraseña diferente a la anterior", 400)
+
+        # Query database para el hash del usuario que cambia la contraseña
+        conexion = db.get_db()
+        user_hash_password = conexion.execute(
+            "SELECT password_hash FROM users WHERE id = ?", (session["user_id"],)
+        ).fetchone()
+
+        # Comprobar la contraseña actual sea la misma que digito
+        if not check_password_hash(user_hash_password["password_hash"], current_password):
+            return apology("Contraseña actual inválida. Su contraseña actual es diferente a la digitada en el campo de contraseña actual. Verifique.", 400)
+
+        # Hash la nueva clave del usuario
+        hash_new_user_password = generate_password_hash(new_password)
+
+        # Update la nueva clave del usuario
+        conexion.execute(
+            "UPDATE users SET password_hash = ? WHERE id = ?", (hash_new_user_password, session["user_id"])
+        )
+
+        # Guardar cambios en la BD
+        conexion.commit()
+
+        # Redirigir el usuario a la pagina home o index
+        return redirect("/")
+
+    else:
+        return render_template("change_password.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+    """Inicia Sesion del usuario"""
 
     # Borra cualquier user_id
     session.clear()
@@ -60,8 +122,10 @@ def login():
         ):
             return apology("El username y/o contraseña es inválido", 400)
 
-        # Recordar que usuario es el que esta en la sesión
+        # Recordar que usuario es el que esta en la sesión y su rol.
         session["user_id"] = resultado_username_login[0]["id"]
+        session["username"] = resultado_username_login[0]["username"]
+        session["user_role"] = resultado_username_login[0]["role"]
 
         # Redirigir el usuario a la pagina home o index
         return redirect("/")
